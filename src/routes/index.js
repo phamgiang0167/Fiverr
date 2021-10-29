c1943042730706453451e8256e52c55afd9eec21
phamgiang0167 <jagsiph@gmail.com>
2021-10-29 19:30:41 +0700

    list Job layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import {lazy} from 'react'
 //client routes
 import Home from '../containers/client/Home/Home'
 import ListJobs from '../containers/client/ListJobs/ListJobs'
+import JobDetails from 'containers/client/JobDetails/JobDetails'
 //admin routes
 import Admin from '../containers/admin/Admin'
 export const clientRoutes = [
@@ -15,6 +16,11 @@ export const clientRoutes = [
     {
         path: '/listjob/by-name',
         component: ListJobs,
+        exact: true
+    },
+    {
+        path: '/job-details/:id',
+        component: JobDetails,
         exact: true
     }
 
